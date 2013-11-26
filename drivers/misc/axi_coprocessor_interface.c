2df7c7d02c457cb85140ab64297ff2484ee6ac9c
Karol Gugala <kgugala@antmicro.com>
2013-11-26 09:22:36 +0100

    second version of axi coprocessor interface driver (memory mapped device)

--- drivers/misc/axi_coprocessor_interface.c
+++ drivers/misc/axi_coprocessor_interface.c
@@ -14,6 +14,8 @@
 
 struct interface_priv{
 	void *mmio;
+	unsigned int buf_in_offs;
+	unsigned int buf_out_offs;
 };
 
 static struct interface_priv interface_private_data;
@@ -32,13 +34,6 @@ static void interface_set_register(struct interface_priv *dev, u32 register_offs
 	*reg = register_value;
 }
 
-static u32 interface_get_data(struct interface_priv *dev)
-{
-	interface_set_register(dev, INTERFACE_CONTROL_REGISTER, INTERFACE_LATCH_FIFO_OUT_DATA_PIN);
-	interface_set_register(dev, INTERFACE_CONTROL_REGISTER, INTERFACE_LATCH_FIFO_OUT_DATA_PIN);
-	return interface_get_register(dev, INTERFACE_FIFO_OUT_REGISTER);
-}
-
 /* char driver api */
 
 static int interface_open(struct inode *inode, struct file *file)
@@ -53,7 +48,7 @@ static int interface_close(struct inode *inode, struct file *file)
 	return 0;
 }
 
-static ssize_t interface_read(struct file *file, const char __user *buffer, size_t length, loff_t *offset)
+static ssize_t interface_read(struct file *file, char __user *buffer, size_t length, loff_t *offset)
 {
 	u32 reg;
 	u32 *buf = (u32*)kmalloc(length + (4 - (length % 4)), GFP_KERNEL);
@@ -62,17 +57,33 @@ static ssize_t interface_read(struct file *file, const char __user *buffer, size
 	
 	if(!buf)
 		return -ENOMEM;
+	/* get driver private data */
 	priv = (struct interface_priv*)file->private_data;
 	
-	reg = interface_get_register(priv, INTERFACE_STATUS_REGISTER);
-	while( (!(reg & INTERFACE_FIFO_OUT_EMPTY_MASK)) && (copied_data < length) )
+	/* copy data from FIFO to out buf */
+	reg = interface_get_register(priv, INTERFACE_CONTROL_REG);
+	reg &= ~(INTERFACE_COPY_DATA_FROM_FIFO);
+	interface_set_register(priv, INTERFACE_CONTROL_REG, reg);
+	reg |= INTERFACE_COPY_DATA_FROM_FIFO;
+	interface_set_register(priv, INTERFACE_CONTROL_REG, reg);
+	
+	/* wait till data is copied  */
+	reg = interface_get_register(priv, INTERFACE_STATUS_REG);
+
+	while(reg & INTERFACE_FIFO_OUT_COPYING_MASK)
 	{
-		buf[copied_data/4] = interface_get_data(priv);
-		copied_data += 4;
-		reg = interface_get_register(priv, INTERFACE_STATUS_REGISTER);
+		reg = interface_get_register(priv, INTERFACE_STATUS_REG);
 	}
 
-	if( copy_to_user(buffer, buf, copied_data) != 0 )
+	/* get data amount */
+	copied_data = interface_get_register(priv, INTERFACE_FIFO_TRANSFER_STATUS_REG);
+	copied_data &= INTERFACE_OUTPUT_DATA_COPIED_MASK;
+	copied_data >>= INTERFACE_OUTPUT_DATA_COPIED_SHIFT;
+	copied_data *= 4;
+
+	memcpy(buf, (void*)(priv->mmio + priv->buf_out_offs), copied_data);
+	
+	if( copy_to_user((void*)buffer, buf, copied_data) != 0 )
 		copied_data =  -EFAULT;	
 
 	kfree(buf);
@@ -82,57 +93,162 @@ static ssize_t interface_read(struct file *file, const char __user *buffer, size
 static ssize_t interface_write(struct file *file, const char __user *buffer, size_t length, loff_t *offset)
 {
 	u32 reg;
+	
 	u32 *buf = (u32*)kmalloc(length + (4 - (length % 4)), GFP_KERNEL); //data must be aligned to 4
-	u32 copied_data = 0;
 	struct interface_priv *priv;
-
+	
 	if(!buf)
-		return -ENOMEM;
-
-	priv = (struct interface_priv*)file->private_data;
-	if( copy_from_user(buf, buffer, length) != 0)
-		length = -EFAULT;
-		
-	reg = interface_get_register(priv, INTERFACE_STATUS_REGISTER);
-	while( (!(reg & INTERFACE_FIFO_IN_FULL_MASK)) && (copied_data < length))
 	{
-		interface_set_register(priv, INTERFACE_FIFO_IN_REGISTER, buf[copied_data/4]);
-		reg = interface_get_register(priv, INTERFACE_STATUS_REGISTER);
-		copied_data += 4;
+			printk(KERN_ERR"alloc error \n");
+			return -ENOMEM;
+	}
+	
+	/* get driver private data */
+	priv = (struct interface_priv*)file->private_data; 
+
+	if( copy_from_user(buf, buffer, length) != 0)
+	{
+		kfree(buf);
+		return -EFAULT;
+	}
+
+	/* copy data to buffer */
+	memcpy( (void*)(priv->mmio + priv->buf_in_offs), buf, length);
+
+	/* push data from buffer into fifo */
+
+	/* set data length */
+	interface_set_register(priv, INTERFACE_FIFO_IN_DATA_AMOUNT_REG, ((length/4) & INTERFACE_IN_DATA_AMOUNT_MASK));
+	/* trigger copy process */
+	reg = interface_get_register(priv, INTERFACE_CONTROL_REG);
+	reg &= ~(INTERFACE_COPY_DATA_TO_FIFO);
+	interface_set_register(priv, INTERFACE_CONTROL_REG, reg);
+	reg |= INTERFACE_COPY_DATA_TO_FIFO;
+	interface_set_register(priv, INTERFACE_CONTROL_REG, reg);
+
+	/* wait until data is copied */
+	reg = interface_get_register(priv, INTERFACE_STATUS_REG);
+	while( reg & INTERFACE_FIFO_IN_COPYING_MASK)
+	{
+		reg = interface_get_register(priv, INTERFACE_STATUS_REG);
 	}
 	kfree(buf);
-	return copied_data;
+
+	/* get real amount of copied data */
+	reg = interface_get_register(priv, INTERFACE_FIFO_TRANSFER_STATUS_REG);
+	reg &= INTERFACE_INPUT_DATA_COPIED_MASK;
+	reg >>=INTERFACE_INPUT_DATA_COPIED_SHIFT; 
+	reg *= 4;
+	return reg;
 }
 
-static int interface_ioctl(struct file *file, unsigned int ioctl_num, unsigned long ioctl_param)
+static long interface_ioctl(struct file *file, unsigned int ioctl_num, unsigned long ioctl_param)
 {
-	struct interface_ioctl_data ioctl_arg;
+	struct interface_ioctl_data ioctl_arg; 	//this is used for setting and getting regs
+	unsigned int ioctl_data;		//this is used for getting FIFO status
 	struct interface_priv *priv;
 
 	priv = (struct interface_priv*) file->private_data;
-	if ( copy_from_user(&ioctl_arg, (struct interface_ioctl_data*) ioctl_param, sizeof(struct interface_ioctl_data)) )
+	
+	/* get data from user space */
+	if(ioctl_num == INTERFACE_SET_USER_REGISTER || ioctl_num == INTERFACE_GET_USER_REGISTER)
 	{
-		return -EACCES;
+		if(copy_from_user(&ioctl_arg, (struct interface_ioctl_data*) ioctl_param, sizeof(struct interface_ioctl_data)))
+	                return -EACCES;
 	}
+	else 
+	{	
+		if(copy_from_user(&ioctl_data, (unsigned int*)ioctl_param, sizeof(unsigned int)))
+			return -EACCES;
+	}
+
 	switch(ioctl_num)
 	{
-		case INTERFACE_SET_REGISTER:
+		case INTERFACE_SET_USER_REGISTER:
+			
+			if( ioctl_arg.register_offset < INTERFACE_USER_REG_0 || ioctl_arg.register_offset > INTERFACE_USER_REG_5 )
+				return -EACCES;
+
 			interface_set_register(priv, (u32)ioctl_arg.register_offset, (u32)ioctl_arg.register_value);
 			break;
-		case INTERFACE_GET_REGISTER:
-			if(ioctl_arg.register_offset == INTERFACE_FIFO_OUT_REGISTER)
-			{
-				ioctl_arg.register_value = interface_get_data(priv);
-			}
-			else ioctl_arg.register_value = interface_get_register(priv, ioctl_arg.register_offset);
+
+		case INTERFACE_GET_USER_REGISTER:
+
+			if( ioctl_arg.register_offset < INTERFACE_USER_REG_0 || ioctl_arg.register_offset > INTERFACE_USER_REG_5 )
+				return -EACCES;
+
+			ioctl_arg.register_value = interface_get_register(priv, ioctl_arg.register_offset);
 
 			if( copy_to_user((struct interface_ioctl_data*)ioctl_param, &ioctl_arg,sizeof(struct interface_ioctl_data)) )
 			{
 				return -EACCES;
 			}
 			break;
+		case INTERFACE_GET_FIFO_IN_EMPTY:
+
+			ioctl_data = interface_get_register(priv, INTERFACE_STATUS_REG);
+			ioctl_data &= INTERFACE_FIFO_IN_EMPTY_MASK;
+			ioctl_data >>= INTERFACE_FIFO_IN_EMPTY_SHIFT;
+			break;
+
+		case INTERFACE_GET_FIFO_IN_FULL:
+
+			ioctl_data = interface_get_register(priv, INTERFACE_STATUS_REG);
+			ioctl_data &= INTERFACE_FIFO_IN_FULL_MASK;
+			ioctl_data >>= INTERFACE_FIFO_IN_FULL_SHIFT;
+			break;
+
+		case INTERFACE_GET_FIFO_IN_COPY_STATUS:
+
+			ioctl_data = interface_get_register(priv, INTERFACE_STATUS_REG);
+			ioctl_data &= INTERFACE_FIFO_IN_COPYING_MASK;
+			ioctl_data >>= INTERFACE_FIFO_IN_COPYING_SHIFT;
+			break;
+
+		case INTERFACE_GET_FIFO_IN_COPIED_DATA_AMOUNT:
+
+			ioctl_data = interface_get_register(priv, INTERFACE_FIFO_TRANSFER_STATUS_REG);
+			ioctl_data &= INTERFACE_INPUT_DATA_COPIED_MASK;
+			ioctl_data >>= INTERFACE_INPUT_DATA_COPIED_SHIFT;
+			break;
+
+		case INTERFACE_GET_FIFO_OUT_EMPTY:
+
+			ioctl_data = interface_get_register(priv, INTERFACE_STATUS_REG);
+			ioctl_data &= INTERFACE_FIFO_OUT_EMPTY_MASK;
+			ioctl_data >>= INTERFACE_FIFO_OUT_EMPTY_SHIFT;
+			break;
+
+		case INTERFACE_GET_FIFO_OUT_FULL:
+
+			ioctl_data = interface_get_register(priv, INTERFACE_STATUS_REG);
+			ioctl_data &= INTERFACE_FIFO_OUT_FULL_MASK;
+			ioctl_data >>= INTERFACE_FIFO_OUT_FULL_SHIFT;
+			break;
+
+		case INTERFACE_GET_FIFO_OUT_COPY_STATUS:
+
+			ioctl_data = interface_get_register(priv, INTERFACE_STATUS_REG);
+			ioctl_data &= INTERFACE_FIFO_OUT_COPYING_MASK;
+			ioctl_data >>= INTERFACE_FIFO_OUT_COPYING_SHIFT;
+			break;
+
+		case INTERFACE_GET_FIFO_OUT_COPIED_DATA_AMOUNT:
+
+			ioctl_data = interface_get_register(priv, INTERFACE_FIFO_TRANSFER_STATUS_REG);
+			ioctl_data &= INTERFACE_OUTPUT_DATA_COPIED_MASK;
+			ioctl_data >>= INTERFACE_OUTPUT_DATA_COPIED_SHIFT;
+			break;
+
 		default:
 			return -EINVAL;
+	}
+	if(ioctl_num != INTERFACE_SET_USER_REGISTER && ioctl_num != INTERFACE_GET_USER_REGISTER)
+	{
+		if(copy_to_user((unsigned int*)ioctl_param, &ioctl_data,sizeof(unsigned int)))
+		{
+			return -EACCES;
+		}
 	}
 	return 0;
 }
@@ -146,30 +262,57 @@ struct file_operations fops = {
 
 /* platform driver api */
 
-static int interface_probe(struct platform_driver *pdev)
+static int interface_probe(struct platform_device *pdev)
 {
 	int ret = 0;
 	struct resource *res;
+	unsigned int dev_space = 0;
+        struct device_node *np = pdev->dev.of_node;
+	unsigned int in_buf_offset;
+	unsigned int out_buf_offset;
 
 	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
 	if (!res)
 	{
-		printk(KERN_ERR"Failed to get device resource\n");
+		printk(KERN_ERR"%s: Failed to get device resource\n", DEVICE_NAME);
 		return -ENODEV;
 	}
 
-	interface_private_data.mmio = ioremap(res->start, INTERFACE_REGISTER_SPACE);
+	dev_space = res->end - res->start;
+
+	if( dev_space < INTERFACE_MINIMAL_REGISTER_SPACE )
+	{
+		printk(KERN_ERR"%s: Insufficient memory space\n", DEVICE_NAME);
+		return -ENODEV;
+	}
+
+	interface_private_data.mmio = ioremap(res->start, dev_space);
 	
+	if( of_property_read_u32(np, "kik,input_buffer_offset", &in_buf_offset))
+	{
+		printk(KERN_ERR"%s: Cannot get input buffer offset\n", DEVICE_NAME);
+		return -ENODEV;
+	}
+
+	if( of_property_read_u32(np, "kik,output_buffer_offset", &out_buf_offset))
+	{
+		printk(KERN_ERR"%s: Cannot get output buffer offset\n", DEVICE_NAME);
+		return -ENODEV;
+	}
+
+	interface_private_data.buf_in_offs = in_buf_offset;
+	interface_private_data.buf_out_offs = out_buf_offset;
+
 	ret = register_chrdev(INTERFACE_MAJOR_NUMBER, DEVICE_NAME, &fops);
 	if (ret < 0)
 	{
-		printk(KERN_ERR"Char device registration failed\n");
+		printk(KERN_ERR"%s: Char device registration failed\n", DEVICE_NAME);
 		return -ENODEV;
 	}
 	return 0;
 }
 
-static int interface_remove(struct platform_driver *pdev)
+static int interface_remove(struct platform_device *pdev)
 {
 	unregister_chrdev(INTERFACE_MAJOR_NUMBER, DEVICE_NAME);
 	return 0;
